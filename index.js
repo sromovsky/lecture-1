241b643af5230a8a4abb37cc88e5a4c9c4906349
MatejK0szoru <Koszorumatej1@gmail.com>
2022-10-07 15:02:50 +0200

    Uloha1 a Uloha2

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 function main() {
-    // Start here ...
+   //Uloha1();
+    // Uloha2()
+}
+
+function Uloha1(){
+    const a = inputA();
+    const b = inputB();
+    const c = inputC();
+    let BMI = a/(b*b);
+    log(BMI);
+    if (BMI <=19) {
+        log("Osoba má podváhu")
+    }else if(BMI > 19 || BMI <= 25) {
+        log("Osoba má normálnu hmotnosť")
+    }else if (BMI > 25 || BMI <= 30){
+        log("Osoba má nadváhu")
+    }
+
+}
+
+function Uloha2(){
+    const a = inputA();
+    const b = inputB();
+    const c = inputC();
 }
