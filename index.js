5a742f7a2eaea472b7aac31b7ea3187c355b4d1c
David Birosik <birosik.david@websupport.sk>
2022-10-07 15:01:09 +0200

    uloha4 a uloha7 hotovo

--- index.js
+++ index.js
@@ -1,3 +1,48 @@
 function main() {
-    // Start here ...
+    //uloha4();
+    //uloha7();
+}
+
+function uloha4() {
+    const a = Number.parseInt(inputA());
+    const b = Number.parseInt(inputB());
+
+    const result = Math.pow(a, b)
+
+    log(result);
+
+}
+
+function uloha7 () {
+    const fix = 1.5
+    const perKm = 0.75
+    const waitPrice = 10
+    let totalPrice;
+
+    const a = Number.parseInt(inputA());
+    const b = Number.parseInt(inputB());
+    const c = inputC().toString().toLowerCase();
+
+    if ( c === "a" ) {
+        totalPrice = 20
+    } else if (c === "n") {
+        totalPrice = 0
+    } else {
+        log("Znečistenie -> a/n")
+    }
+
+    if( a <= 5 ) {
+        totalPrice += fix
+    } else {
+        totalPrice += fix + (a * perKm)
+    }
+
+    if( b > 0) {
+        totalPrice += b * (waitPrice/60)
+    } else {
+        totalPrice += 0
+    }
+
+    log(totalPrice.toFixed(2));
+
 }
