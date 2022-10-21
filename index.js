546417ec631a5f164250a809e739d8283f939e63
quib963 <55764690+quib963@users.noreply.github.com>
2022-10-21 10:32:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,40 @@
 function main() {
-    // Start here ...
+    //task4();
+	task6();
+}
+
+function task4() {
+    const a = Number.parseInt(inputA());
+    const b = Number.parseInt(inputB());
+    //const c = inputC();
+let c = Math.pow(a,b)
+	log(c);
+
+}
+function task6() {
+    const a = Number.parseInt(inputA());
+
+let c=1;
+
+var x=1;
+
+if (a>0) {
+	
+
+do 
+{
+	x*=c;
+	//log(x);
+	c++;
+	//log(c);
+}
+while(c <= a);
+log(x);
+
+	}
+else
+{
+	log ("Zadaj celé číslo > 0");
+
+}
 }
